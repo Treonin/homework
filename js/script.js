37fb52dbbb49d5de4901b80037b3f3c3778a7ec3
Treonin <v.techno.od.ua@gmail.com>
2022-03-22 02:17:13 +0200

    add .gitignore .prettier.json

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict"; // Написання коду в строгому стилі
+
+//const isComing = prompt("Please confirm hotel reservation");
+//console.log(isComing);
+//alert(isComing);
+//const username = "Mango";
+//console.log("Username is ", username); // Username is Mango
+const message = "JavaScript is awesome!";
+alert(message);
